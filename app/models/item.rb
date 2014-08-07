06ee8b02d6faaab2736a84adc8a88dd601b9eed7
Tim Sullivan <tim@withoutsoftware.com>
2014-08-07 18:46:16 -0400

    argh.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   belongs_to :user
-  belongs_to :created_by, class: 'User', foreign_key: 'created_by_user_id'
+  belongs_to :created_by, class_name: 'User', foreign_key: 'created_by_user_id'
 
   scope :incomplete, -> { where('completed_at is null') }
 
